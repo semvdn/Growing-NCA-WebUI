6f240fb8e944724597b5f42a89d09c435f4b2fdb
Sem van den Nieuwendijk <semvdn@gmail.com>
2025-08-11 13:51:43 +0200

    change target size to 48x48

--- nca_globals.py
+++ nca_globals.py
@@ -2,8 +2,8 @@
 """Global constants for the NCA application."""
 
 CHANNEL_N = 16
-TARGET_SIZE = 40 # This is the internal size of the target after processing
-TARGET_PADDING = 16
+TARGET_SIZE = 48 # This is the internal size of the target after processing
+TARGET_PADDING = 8
 DEFAULT_FIRE_RATE = 0.5
 DEFAULT_BATCH_SIZE = 8
 DEFAULT_POOL_SIZE = 1024
@@ -15,7 +15,7 @@ MAX_ENTROPY_STRENGTH = 0.1
 
 HISTORY_MAX_SIZE = 200
 DEFAULT_RUNNER_SLEEP_DURATION = 0.05
-RUNNER_SLEEP_DURATION_TRAINING_ACTIVE = 0.2 # Slower FPS when training is active (e.g., 5 FPS)
+RUNNER_SLEEP_DURATION_TRAINING_ACTIVE = 1 # Slower FPS when training is active (e.g., 5 FPS)
 
 # New for drawn patterns (client-side canvas size for drawing target)
 # This should match the display size of the drawing canvas on the trainer tab.
